b106523dd4984c51e60ba2e8ba9dcfe7c110db0c
matJavaDev <mateusz.smiech@gmail.com>
2018-06-21 20:24:45 +0200

    Add dependency to hibernate and mysql

---
+++ src/main/java/pl/kpir/kpir/kpir/model/UserEntiry.java
@@ -0,0 +1,13 @@
+package pl.kpir.kpir.kpir.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class UserEntiry {
+}
