27c44f853993eec149886d80886015af0b9808c8
matJavaDev <mateusz.smiech@gmail.com>
2018-06-21 19:36:33 +0200

    Ignore application-ext.properties

--- src/main/java/pl/kpir/kpir/KpirApplication.java
+++ src/main/java/pl/kpir/kpir/KpirApplication.java
@@ -2,8 +2,10 @@ package pl.kpir.kpir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource( value = {"application-ext.properties"})
 public class KpirApplication {
 
 	public static void main(String[] args) {
