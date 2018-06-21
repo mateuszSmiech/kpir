474bfce71fb54ed99d4c2ad08e5c771f3ecd5d6e
matJavaDev <mateusz.smiech@gmail.com>
2018-06-21 18:39:59 +0200

    Initial commit

---
+++ src/main/java/pl/kpir/kpir/KpirApplication.java
@@ -0,0 +1,12 @@
+package pl.kpir.kpir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpirApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/kpir/kpir/KpirApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kpir.kpir;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KpirApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
