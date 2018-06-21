92af750c552b5bf6b0f8fcff1ff9e38fe21101fa
matJavaDev <mateusz.smiech@gmail.com>
2018-06-21 20:48:37 +0200

    Add UserEntity

---
+++ src/main/java/pl/kpir/kpir/kpir/model/UserEntity.java
@@ -0,0 +1,30 @@
+package pl.kpir.kpir.kpir.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    @Column(name="birth_date")
+    private LocalDate birthDate;
+
+
+}
