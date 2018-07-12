5d79f15248fc74aab379823877483ab58dd9d98e
matJavaDev <mateusz.smiech@gmail.com>
2018-07-12 23:08:02 +0200

    Add validation for company registration.

--- src/main/java/pl/kpir/kpir/kpir/controller/CompanyController.java
+++ src/main/java/pl/kpir/kpir/kpir/controller/CompanyController.java
@@ -3,12 +3,15 @@ package pl.kpir.kpir.kpir.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.kpir.kpir.kpir.forms.CreateCompanyForm;
 import pl.kpir.kpir.kpir.services.CompanyEntityService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/company")
@@ -29,7 +32,10 @@ public class CompanyController {
     }
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
-    public String addCompany(@ModelAttribute CreateCompanyForm createCompanyForm) {
+    public String addCompany(@ModelAttribute("createCompany") @Valid CreateCompanyForm createCompanyForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "registerCompany";
+        }
         companyEntityService.saveCompany(createCompanyForm);
         return "redirect:/dashboard";
     }
--- src/main/java/pl/kpir/kpir/kpir/forms/CreateCompanyForm.java
+++ src/main/java/pl/kpir/kpir/kpir/forms/CreateCompanyForm.java
@@ -4,20 +4,37 @@ package pl.kpir.kpir.kpir.forms;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.pl.NIP;
+import org.hibernate.validator.constraints.pl.REGON;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class CreateCompanyForm {
+    @NotEmpty(message = "{validation.shortName.NotEmpty}")
     private String shortCompanyName;
+    @NotEmpty(message = "{validation.fullName.NotEmpty}")
     private String fullCompanyName;
+    @NotEmpty(message = "{validation.nip.NotEmpty}")
+    @NIP(message = "{validation.nip.format}")
     private String nip;
+    @REGON(message = "{validation.regon.format}")
     private String regon;
+    @NotEmpty(message = "{validation.street.NotEmpty}")
     private String street;
+    @NotEmpty(message = "{validation.postCode.NotEmpty}")
+    @Pattern(regexp = "\\d{2}-\\d{3}", message = "{validation.postCode.pattern}")
     private String postCode;
+    @NotEmpty(message = "{validatino.city.NotEmpty}")
     private String city;
     private String country;
     private String telephoneNumber;
+    @Email(message = "{validation.email.wrongFormat}")
     private String email;
     private boolean vat;
     private String taxForm;
