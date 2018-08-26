5188fedee1212e4c9fee9997e19d01656c94f591
Mateusz <mateusz.smiech@gmail.com>
2018-08-26 20:17:52 +0200

    Set current month in the month dropdown in the entire project.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,10 +1,26 @@
 //datepicker
-$( function() {
+$(
+    function() {
 
     $( "#exampleConfirmPassword" ).datepicker({
         dateFormat: "yy-mm-dd"
     });
+
 });
+
+//load current month
+var get_url= window.location.href;
+var url = new URL(get_url);
+var paramValue = url.searchParams.get("month");
+
+function SelectElement(id, valueToSelect)
+{
+    var element = document.getElementById(id);
+    element.value = valueToSelect;
+}
+
+SelectElement("month", paramValue)
+
 
 
 
